bd2bfeb6d37a25c9460ac042e5b2f12ff2833455
JulienZammit <78787470+JulienZammit@users.noreply.github.com>
2025-10-02 11:56:14 +0200

    fix(sql): Remediate SQL injection in password_change function

--- bad/libuser.py
+++ bad/libuser.py
@@ -50,7 +50,7 @@ def password_change(username, password):
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
 
-    c.execute("UPDATE users SET password = '{}' WHERE username = '{}'".format(password, username))
+    c.execute("UPDATE users SET password = :password WHERE username = :username", {'password': password, 'username': username})
     conn.commit()
 
     return True
